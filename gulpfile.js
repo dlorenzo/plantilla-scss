f6b13c1ad230957b8556ab3e5f322a961455b7a7
Daniel Lorenzo <daniel.lorenzoabalo@plexus.es>
2018-03-22 08:40:38 +0100

    Actualización de dependencias y rutas configurables.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var sass = require('gulp-sass');
 var wait = require('gulp-wait');
 var sourcemaps = require('gulp-sourcemaps');
 var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var gulpif = require('gulp-if');
+var package = require('./package.json');
+
+// Paths
+var scss = package.config.scss;
+var css = package.config.css;
 
 gulp.task('build', ['clean'], function () {
   procSass(true);
@@ -10,31 +18,30 @@ gulp.task('build', ['clean'], function () {
 
 gulp.task('watch', ['clean'], function () {
   procSass();
-  gulp.watch('./scss/{,**/}*.{scss,sass}', ['sass']);
+  gulp.watch(scss + '/{,**/}*.{scss,sass}', ['sass']);
 });
 
 gulp.task('clean', function () {
-  return gulp.src('./css/*', { read: false })
+  gulp.src([], { read: false })
     .pipe(clean());
 });
 
-gulp.task('sass', procSass);
+gulp.task('sass', function() {
+  procSass();
+});
 
 /**
  * Estilos
  */
-function procSass(release = false) {
-  if (release === true) {
-    return gulp.src('./scss/**/*.scss')
-      .pipe(wait(500))
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-  } else {
-    return gulp.src('./scss/**/*.scss')
-      .pipe(wait(1000))
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./css'));
-  }
+function procSass(release) {
+  gulp.src(scss + '/**/*.scss')
+    // RELEASE
+    .pipe(gulpif(release, wait(500)))
+    .pipe(gulpif(release, sass({ outputStyle: 'compressed' }).on('error', sass.logError)))
+    // DEBUG
+    .pipe(gulpif(!release, wait(1000)))
+    .pipe(gulpif(!release, sourcemaps.init()))
+    .pipe(gulpif(!release, sass().on('error', sass.logError)))
+    .pipe(gulpif(!release, sourcemaps.write()))
+    .pipe(gulp.dest(css));
 }
